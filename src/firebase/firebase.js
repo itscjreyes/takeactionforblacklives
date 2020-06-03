b0729db68c9424a0f03c463997079fcaaf1ac12f
CJ Reyes <itscjreyes@gmail.com>
2020-06-03 15:05:56 -0400

    Firebase key

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,16 @@
 import firebase from 'firebase';
 
+const key = process.env.FIREBASE_KEY;
+const id = process.env.FIREBASE_ID;
+
 const config = {
-    apiKey: "AIzaSyAPBaC-KJV0nlXHI-tsMt0Df4vG1DdjAtI",
+    apiKey: key,
     authDomain: "tabl-9c663.firebaseapp.com",
     databaseURL: "https://tabl-9c663.firebaseio.com",
     projectId: "tabl-9c663",
     storageBucket: "tabl-9c663.appspot.com",
     messagingSenderId: "1018707962108",
-    appId: "1:1018707962108:web:32e9dbf983eb7d1a4b4f79",
+    appId: id,
     measurementId: "G-SVE9MJNCHZ"
 };
 
